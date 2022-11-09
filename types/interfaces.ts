8bd89232c9488c1904fd148c9872bc879ab82bc5
dyh_a <xinzon32177@163.com>
2022-11-10 01:25:53 +0800

    chore: 提取公共类型

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -1,4 +1,11 @@
-export interface MDDirectory{
-    level:number;
-    value:string;
+export interface MDDirectory {
+  level: number;
+  value: string;
+}
+
+export interface MDFile {
+  name: string;
+  originContent: string;
+  content: string;
+  path: string;
 }
