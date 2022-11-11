874b89b029c679f6ef40a0ed06c459629d27f414
dyh <xinzon32177@163.com>
2022-11-11 13:32:48 +0800

    feat: 添加prettier格式化md功能
    Closes #8

--- src/store/markdown.ts
+++ src/store/markdown.ts
@@ -2,17 +2,20 @@ import { defineStore } from 'pinia';
 import { ipcRenderer } from 'electron';
 import { MDFile } from '../../types/interfaces';
 import { computed, reactive, toRefs, watch } from 'vue';
+import prettier from 'prettier';
+import parserMD from 'prettier/esm/parser-markdown.mjs';
+import parserTs from 'prettier/esm/parser-typescript.mjs';
+import parserBabel from 'prettier/esm/parser-babel.mjs';
+import parserHTML from 'prettier/esm/parser-html.mjs';
+import parserAngular from 'prettier/esm/parser-angular.mjs';
+import parserFlow from 'prettier/esm/parser-flow.mjs';
+import parserGraphql from 'prettier/esm/parser-graphql.mjs';
+import parserPostcss from 'prettier/esm/parser-postcss.mjs';
+import parserYaml from 'prettier/esm/parser-yaml.mjs';
 
 let isWatched = false;
 export const useMarkdownStore = defineStore('md-file-store', () => {
   const state = reactive({ name: '', originContent: '', content: '', path: '' });
-
-  // watch(
-  //   state,
-  //   debounce((n: MDFile) => {
-  //     ipcRenderer.send('md-store-update', { ...n });
-  //   }, 100),
-  // );
 
   const isModify = computed(() => {
     const { content, path, originContent } = state;
@@ -20,7 +23,9 @@ export const useMarkdownStore = defineStore('md-file-store', () => {
     return originContent !== content;
   });
 
-  watch(isModify, (n) => ipcRenderer.send('set-can-close', !n), { immediate: true });
+  watch(isModify, (n) => ipcRenderer.send('set-can-close', !n), {
+    immediate: true,
+  });
 
   const actions = reactive({
     save(): void {
@@ -115,6 +120,55 @@ export const useMarkdownStore = defineStore('md-file-store', () => {
         reply();
       },
     );
+
+    // 格式化markdown
+    ipcRenderer.on('format-md', () => {
+      state.content = prettier.format(state.content, {
+        parser: 'markdown',
+        plugins: [
+          parserMD,
+          parserTs,
+          parserHTML,
+          parserBabel,
+          parserAngular,
+          parserFlow,
+          parserGraphql,
+          parserPostcss,
+          parserYaml,
+        ],
+
+        // 以下来自.prettierrc.js
+
+        // 一行最多 100 字符
+        printWidth: 100,
+        // 使用 2 个空格缩进
+        tabWidth: 2,
+        // 不使用缩进符使用空格
+        useTabs: false,
+        // 行尾需要有分号
+        semi: true,
+        // 使用单引号
+        singleQuote: true,
+        // 对象的 key 仅在必要时用引号
+        quoteProps: 'as-needed',
+        // jsx 不使用单引号，而使用双引号
+        jsxSingleQuote: false,
+        // 尾随逗号
+        trailingComma: 'all',
+        // 大括号内的首尾需要空格
+        bracketSpacing: true,
+        // jsx 标签的反尖括号需要换行
+        jsxBracketSameLine: false,
+        // 箭头函数，只有一个参数的时候，也需要括号
+        arrowParens: 'always',
+        // 使用默认的折行标准
+        proseWrap: 'preserve',
+        // 根据显示样式决定 html 要不要折行
+        htmlWhitespaceSensitivity: 'css',
+        // 换行符使用 auto
+        endOfLine: 'auto',
+      });
+    });
   }
 
   addListener();
